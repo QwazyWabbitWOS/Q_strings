f66fc2d4bec91e6c834773e0e41bd14c42fe1128
QwazyWabbit <gandalf.2004@gmail.com>
2022-11-14 11:07:31 -0800

    Add project files.

---
+++ Qstrings.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#ifdef _WIN32
+#pragma warning(disable : 4996)	// deprecated CRT functions (_CRT_SECURE_NO_WARNINGS).
+#endif
+
+// These two functions courtesy Knightmare.
+
+// Safely copies source string into destination string.
+// Returns the number of characters copied into the destination
+// if successful or returns the number of characters that would have
+// been copied if there was room in the destination. Returns 0 on bad input.
+// Always nul-terminates the destination.
+size_t Q_strncpyz(char* dst, size_t dstSize, const char* src)
+{
+    char* d = dst;
+    const char* s = src;
+    size_t        decSize = dstSize;
+
+    if (!dst || !src || dstSize < 1) {
+        return 0;
+    }
+
+    while (--decSize && *s)
+        *d++ = *s++;
+    *d = 0;
+
+    if (decSize == 0)    // Insufficent room in dst, return count + length of remaining src
+        return (s - src + strlen(s));
+    else
+        return (s - src);    // returned count excludes NUL terminator
+}
+
+// Safely concatenates C strings.
+// Returns the number of characters in the destination if successful. 
+// Returns the number of characters that would have been in the destination
+// if there was room in the destination. Returns 0 on bad input.
+// Always nul-terminates the destination.
+size_t Q_strncatz(char* dst, size_t dstSize, const char* src)
+{
+    char* d = dst;
+    const char* s = src;
+    size_t        decSize = dstSize;
+    size_t        dLen;
+
+    if (!dst || !src || dstSize < 1) {
+        return 0;
+    }
+
+    while (--decSize && *d)
+        d++;
+    dLen = d - dst;
+
+    if (decSize == 0)
+        return (dLen + strlen(s));
+
+    if (decSize > 0) {
+        while (--decSize && *s)
+            *d++ = *s++;
+
+        *d = 0;
+    }
+
+    return (dLen + (s - src));    // returned count excludes NULL terminator
+}
+
+int main(int argc, char* argv[])
+{
+    size_t count;
+    char dest[50];
+    const char* src0 = "This proposed string is way too long for the destination.";
+    const char* src1 = "magic ";
+    const char* src2 = "eightball";
+    const char* src3 = " says \"You may rely on it.\"";
+
+    printf("Begin testing Q_strncpyz and Q_strncatz.\n\n");
+    count = Q_strncpyz(NULL, sizeof dest, src0); // Bad first arg
+    assert(count == 0);
+    count = Q_strncpyz(dest, 0, src0); // Bad second arg
+    assert(count == 0);
+    count = Q_strncpyz(dest, sizeof dest, NULL); // Bad third arg
+    assert(count == 0);
+
+    count = Q_strncatz(NULL, sizeof dest, src0); // Bad first arg
+    assert(count == 0);
+    count = Q_strncatz(dest, 0, src0); // Bad second arg
+    assert(count == 0);
+    count = Q_strncatz(dest, sizeof dest, NULL); // Bad third arg
+    assert(count == 0);
+
+    printf("Test truncation modes.\n");
+    count = Q_strncpyz(dest, sizeof dest, src0);
+    assert(count == strlen(src0));
+    if (strlen(dest) < strlen(src0)) {
+        printf("\"%s\"\n\"%s\"\n", dest, src0);
+        puts("Q_strncpyz OK\n");
+    }
+
+    count = Q_strncatz(dest, sizeof dest, src0);
+    assert(count == strlen(dest) + strlen(src0));
+    if (strlen(dest) < strlen(src0)) {
+        printf("\"%s\"\n\"%s\"\n", dest, src0);
+        puts("Q_strncatz OK\n");
+    }
+
+    /* Normal pathways */
+    count = Q_strncpyz(dest, sizeof dest, src1);
+    assert(count == strlen(src1));
+    printf("%s\n", dest);
+    count = Q_strncatz(dest, sizeof dest, src2);
+    assert(count == strlen(dest));
+    printf("%s\n", dest);
+    count = Q_strncatz(dest, sizeof dest, src3);
+    assert(count == strlen(dest));
+    printf("%s\n\n", dest);
+
+    printf("Success testing Q_strncpyz and Q_strncatz.\n");
+}
+
+/*
+Begin testing Q_strncpyz and Q_strncatz.
+
+Test truncation modes.
+"This proposed string is way too long for the dest"
+"This proposed string is way too long for the destination."
+Q_strncpyz OK
+
+"This proposed string is way too long for the dest"
+"This proposed string is way too long for the destination."
+Q_strncatz OK
+
+magic
+magic eightball
+magic eightball says "You may rely on it."
+
+Success testing Q_strncpyz and Q_strncatz.
+
+*/
